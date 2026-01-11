729c3a60e5d7c8e783600a6ddf0ab148d33f84df
ReemaFathima <reemafathima279@gmail.com>
2026-01-11 21:16:01 +0530

    Refactor showCourses function and add showEnrolledStudents

--- myscript.js
+++ myscript.js
@@ -17,3 +17,21 @@ function showCourses(){
         });
     });
 }
+function showEnrolledStudents(){
+     fetch("http://localhost:8080/courses/enrolled")
+    .then((response) => response.json())
+    .then((students) => {
+        const dataTable = document.getElementById("enrolledtable")
+
+        students.forEach(student=> {
+            var row = `<tr>
+                <td>${student.name}</td>
+                <td>${student.emailId}</td>
+                <td>${student.courseName}</td>
+             
+            </tr>`
+
+            dataTable.innerHTML+=row;
+        });
+    });
+}
